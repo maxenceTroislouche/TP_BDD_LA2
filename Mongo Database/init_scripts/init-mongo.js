af7ffd427ed279e9054482d4b13b32fb60b8ae96
timothe <timotheboone@gmail.com>
2024-02-17 14:10:43 +0100

    Patch init-mongo

--- Mongo Database/init_scripts/init-mongo.js
+++ Mongo Database/init_scripts/init-mongo.js
@@ -1,14 +1,13 @@
-db = connect("mongodb://localhost/CM_IMMO");
+db = connect("mongodb://localhost/immoDB");
 
 // let json_auth = require("/docker-entrypoint-initdb.d/data/auth.json");
 let json_comments = require("/docker-entrypoint-initdb.d/data/comments.json");
-// let json_locations = require("/docker-entrypoint-initdb.d/data/locations.json");
+let json_locations = require("/docker-entrypoint-initdb.d/data/locations.json");
 let json_q_a = require("/docker-entrypoint-initdb.d/data/Q&A.json");
 // let json_users = require("/docker-entrypoint-initdb.d/data/users.json");
 
 // db.credentials.insertMany(json_auth);
-db.comments.insertMany(json_comments.positif);
-db.comments.insertMany(json_comments.negatif);
-// db.bien.insertMany(json_locations);
+db.comments.insertMany(json_comments);
+db.goods.insertMany(json_locations);
 db.question.insertMany(json_q_a);
 // db.user.insertMany(json_users);
