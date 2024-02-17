49c8c217c799ed83778e4f95f3a6e5e8dfbfc4a4
timothe <timotheboone@gmail.com>
2024-02-17 14:52:01 +0100

    Script JSON for Mongo

---
+++ Mongo Database/createDataObjectFormat.js
@@ -0,0 +1,99 @@
+import jsonLocations from "./init_scripts/data/locations.json" with { type: "json" };
+import jsonUser from "./init_scripts/data/users.json" with { type: "json" };
+import jsonComments from "./init_scripts/data/comments.json" with { type: "json" };
+import jsonQuestions from "./init_scripts/data/Q&A.json" with { type: "json" };
+import { writeFile } from "node:fs";
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+const generateJS = (nbBiens, filepath) => {
+
+    // Read location file
+    // For each document, add : etage, num_appartement, date_creation, surface, type eau chaude, type chauffage, classe bien, type bien, nb_pieces, type_location
+
+    let jsonBiens=[];
+    //let jsonLocationsParsed = JSON.parse(jsonLocations);
+
+    jsonLocations.forEach(location => {
+
+        location.etage = getRandomInt(10);
+
+        location.numAppartement = getRandomInt(10);
+
+        let creationDate = getRandomInt(1708168084);
+        location.dateCreation = new Date(creationDate);
+
+        location.surface = getRandomInt(150);
+
+        const typesEnergie = ["Électricité","Gaz","Fioul","Énergie solaire"];
+        location.typeEauChaude = typesEnergie[getRandomInt(4)];
+        location.typeChauffage = typesEnergie[getRandomInt(4)];
+        
+        const classesBien = ["T1","T2","T3","T4","T5","T6","Duplex","Triplex","Souplex","Loft"];
+        location.classeBien = classesBien[getRandomInt(10)];
+
+        const typesBien = ["Appartement", "Maison", "Local commercial"];
+        location.typeBien = typesBien[getRandomInt(3)];
+
+        location.nbPieces = getRandomInt(10);
+
+        const typesLocation = ["Location longue", "Location courte"];
+        location.typeLocation = typesLocation[getRandomInt(2)];
+
+        // Add questions
+        location.questions = [];
+        for (let i = 0; i < getRandomInt(6); i++) {
+            location.questions.push(jsonQuestions[getRandomInt(jsonQuestions.length)]);
+        }
+
+        // Add reservation
+        if (location.typeLocation == "Location courte") {
+            location.reservations = [];
+            for (let i = 0; i < getRandomInt(10); i++) {
+                let reservation={locataire:{}};
+
+                // Add third
+                reservation.locataire.nom = jsonUser[getRandomInt(jsonUser.length)].nom;
+                reservation.locataire.prenom = jsonUser[getRandomInt(jsonUser.length)].prenom;
+                reservation.locataire.dateDeNaissance = new Date(jsonUser[getRandomInt(jsonUser.length)].date_de_naissance );
+                reservation.locataire.typeTier = "Locataire";
+
+                // Add arrival date and departure date
+                const arrivalDate = creationDate + getRandomInt(1708168084);
+                const departureDate = arrivalDate + getRandomInt(2629743);  // One month max
+
+                reservation.dateDebut = new Date( arrivalDate);
+                reservation.dateFin = new Date( departureDate );
+
+                // Add amount
+                reservation.montant = getRandomInt(2000);
+
+                // Add comments
+                if(getRandomInt(2)){
+                    reservation.commentaire = jsonComments[getRandomInt(jsonComments.length)];
+                }
+                location.reservations.push(reservation);
+            }
+
+            
+        }
+
+        
+        jsonBiens.push(location);
+        //console.log(jsonBiens);
+    });
+
+    writeFile("./init_scripts/data/bien_Mongo.json", JSON.stringify(jsonBiens), { encoding: 'utf-8' }, err => {
+        if (err) {
+            console.error(`Erreur lors de l'écriture dans le fichier ${filepath}`);
+            process.exit(1);
+        } else {
+            console.log(`Création du JSON réussie`);
+        }
+    });
+    
+}
+
+generateJS();
--- Mongo Database/init_scripts/init-mongo.js
+++ Mongo Database/init_scripts/init-mongo.js
@@ -2,12 +2,12 @@ db = connect("mongodb://localhost/immoDB");
 
 // let json_auth = require("/docker-entrypoint-initdb.d/data/auth.json");
 let json_comments = require("/docker-entrypoint-initdb.d/data/comments.json");
-let json_locations = require("/docker-entrypoint-initdb.d/data/locations.json");
+let json_biens = require("/docker-entrypoint-initdb.d/data/bien_Mongo.json");
 let json_q_a = require("/docker-entrypoint-initdb.d/data/Q&A.json");
 // let json_users = require("/docker-entrypoint-initdb.d/data/users.json");
 
 // db.credentials.insertMany(json_auth);
 db.comments.insertMany(json_comments);
-db.goods.insertMany(json_locations);
+db.biens.insertMany(json_biens);
 db.question.insertMany(json_q_a);
 // db.user.insertMany(json_users);
